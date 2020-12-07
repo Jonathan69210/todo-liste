c32cf60452e8f8e0ed4c4159983b431909bde814
Jonathan69210 <joe.radig@gmail.com>
2020-12-07 12:13:37 +0100

    Initialize repository

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  jsonbin: {
-    key: "$2b$10$zmOSnz.aY5M7AY1Jc.BFwOdgwTYl8520t56SGcxLgGQh.5eOJfCMe",
-    url: "https://api.jsonbin.io/b/5fc1039c045eb86f1f87e057"
-  }
-};
